84e9c4cef27486bf03092bfb04256f5f753d3681
GM-RND <gdaud175@gmail.com>
2024-10-07 13:22:19 +0300

    INIT2

--- VR-TOUR/data.js
+++ VR-TOUR/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bar-view",
-      "name": "BAR VIEW",
+      "id": "0-view-1",
+      "name": "View 1",
       "levels": [
         {
           "tileSize": 256,
@@ -28,49 +28,12 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.788542190042465,
-          "pitch": -0.04718477774590468,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        },
-        {
-          "yaw": 1.324608769028842,
-          "pitch": -0.011297100896777934,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        },
-        {
-          "yaw": 1.7405363625075818,
-          "pitch": 0.12762728146761404,
-          "rotation": 0,
-          "target": "7-view-2"
-        },
-        {
-          "yaw": 2.3971032402628243,
-          "pitch": 0.1389430066474482,
-          "rotation": 0,
-          "target": "8-vip"
-        },
-        {
-          "yaw": -2.142235174907956,
-          "pitch": 0.1720262660815024,
-          "rotation": 0,
-          "target": "5-entrance-side-view"
-        },
-        {
-          "yaw": -2.127901010042292,
-          "pitch": 0.45727730946616774,
-          "rotation": 0,
-          "target": "6-view-1"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "1-exterior-view-1",
-      "name": "EXTERIOR VIEW 1",
+      "id": "1-view-2",
+      "name": "View 2",
       "levels": [
         {
           "tileSize": 256,
@@ -96,19 +59,12 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.11715198550778716,
-          "pitch": 0.138053332033806,
-          "rotation": 0,
-          "target": "2-exterior-view-2"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-exterior-view-2",
-      "name": "EXTERIOR VIEW 2",
+      "id": "2-view-3",
+      "name": "View 3",
       "levels": [
         {
           "tileSize": 256,
@@ -134,19 +90,12 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.18011425559863525,
-          "pitch": 0.13033604703922208,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "3-female-bathroom",
-      "name": "FEMALE BATHROOM",
+      "id": "3-view-4",
+      "name": "View 4",
       "levels": [
         {
           "tileSize": 256,
@@ -172,19 +121,12 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -1.2115860257166648,
-          "pitch": 0.07292902526090472,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "4-male-bathroom",
-      "name": "MALE BATHROOM",
+      "id": "4-view-5",
+      "name": "View 5",
       "levels": [
         {
           "tileSize": 256,
@@ -210,294 +152,15 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.995740810828984,
-          "pitch": 0.028741367647874938,
-          "rotation": 0,
-          "target": "3-female-bathroom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-entrance-side-view",
-      "name": "ENTRANCE SIDE VIEW",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.062096302023427086,
-          "pitch": -0.057560070944450814,
-          "rotation": 0,
-          "target": "0-bar-view"
-        },
-        {
-          "yaw": 0.11955518389759234,
-          "pitch": 0.09459239151708232,
-          "rotation": 0,
-          "target": "6-view-1"
-        },
-        {
-          "yaw": 0.6100052682366748,
-          "pitch": 0.009624876721197495,
-          "rotation": 0,
-          "target": "7-view-2"
-        },
-        {
-          "yaw": 0.1388536471777968,
-          "pitch": -0.0979826967710693,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        },
-        {
-          "yaw": 0.41645520496185284,
-          "pitch": -0.08106593086424319,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        },
-        {
-          "yaw": 0.9165215007692282,
-          "pitch": -0.0008207187819948558,
-          "rotation": 0,
-          "target": "8-vip"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-view-1",
-      "name": "VIEW 1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.5594644832990454,
-          "pitch": 0.009409834773624226,
-          "rotation": 0,
-          "target": "0-bar-view"
-        },
-        {
-          "yaw": -0.31062036395395864,
-          "pitch": -0.10761978257622573,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        },
-        {
-          "yaw": 0.040891480027751115,
-          "pitch": -0.07007217326885495,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        },
-        {
-          "yaw": 0.3511456629861094,
-          "pitch": 0.062423156306055105,
-          "rotation": 0,
-          "target": "7-view-2"
-        },
-        {
-          "yaw": 0.8674830061147372,
-          "pitch": 0.05321799856301368,
-          "rotation": 0,
-          "target": "8-vip"
-        },
-        {
-          "yaw": 2.977617168964832,
-          "pitch": 0.24515965350609648,
-          "rotation": 0,
-          "target": "5-entrance-side-view"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "7-view-2",
-      "name": "VIEW 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.29735457987308145,
-          "pitch": 0.0021230722788718737,
-          "rotation": 0,
-          "target": "0-bar-view"
-        },
-        {
-          "yaw": -1.6989645246212604e-9,
-          "pitch": 0.03551430615956619,
-          "rotation": 0,
-          "target": "6-view-1"
-        },
-        {
-          "yaw": -0.15386559109212783,
-          "pitch": 0.04922022613861721,
-          "rotation": 0,
-          "target": "5-entrance-side-view"
-        },
-        {
-          "yaw": -0.9621077159427571,
-          "pitch": 0.14250100358710682,
-          "rotation": 0,
-          "target": "8-vip"
-        },
-        {
-          "yaw": 1.107939644745871,
-          "pitch": -0.13350650972918388,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        },
-        {
-          "yaw": 2.098693354472082,
-          "pitch": -0.20512645237112892,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-vip",
-      "name": "VIP",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.46336532740224,
-          "pitch": 0.1714655547144659,
-          "rotation": 0,
-          "target": "7-view-2"
-        },
-        {
-          "yaw": 2.066968392055063,
-          "pitch": 0.1081835167770766,
-          "rotation": 0,
-          "target": "6-view-1"
-        },
-        {
-          "yaw": 2.4483624523789054,
-          "pitch": 0.050192697368707684,
-          "rotation": 0,
-          "target": "0-bar-view"
-        },
-        {
-          "yaw": 1.7954228930531393,
-          "pitch": 0.1046654484293601,
-          "rotation": 0,
-          "target": "5-entrance-side-view"
-        },
-        {
-          "yaw": 3.140466890826472,
-          "pitch": -0.015935537515099796,
-          "rotation": 0,
-          "target": "4-male-bathroom"
-        },
-        {
-          "yaw": -2.491088590696478,
-          "pitch": -0.038412722571553815,
-          "rotation": 0,
-          "target": "1-exterior-view-1"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "SHEGEN 1",
+  "name": "GM-AUDITORIUM",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
