c7490db9e0d6ad87e7c64d5530917c017a451131
GM-RND <gdaud175@gmail.com>
2024-12-21 10:15:58 +0300

    df

--- VR-TOUR3/data.js
+++ VR-TOUR3/data.js
@@ -156,69 +156,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "5-master-bedroom",
-      "name": "Master Bedroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-master-bathroom",
-      "name": "Master Bathroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "7-bedroom-2-view-a",
+      "id": "5-bedroom-2-view-a",
       "name": "Bedroom 2 View A",
       "levels": [
         {
@@ -249,8 +187,70 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "8-bedroom-2-view-b",
+      "id": "6-bedroom-2-view-b",
       "name": "Bedroom 2 View B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-master-bedroom",
+      "name": "Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-master-bathroom",
+      "name": "Master Bathroom",
       "levels": [
         {
           "tileSize": 256,
@@ -280,7 +280,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Ayat Interior",
+  "name": "Ayat interior",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
