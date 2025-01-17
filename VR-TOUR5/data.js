c02b18da29766883d6f8a21bebac6b9bccad7c78
GM-RND <gdaud175@gmail.com>
2025-01-17 10:55:30 +0300

    kc

--- VR-TOUR5/data.js
+++ VR-TOUR5/data.js
@@ -1,8 +1,39 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-kitchen-cabinet",
-      "name": "Kitchen cabinet",
+      "id": "0-option-2",
+      "name": "Option 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-option-1",
+      "name": "Option 1",
       "levels": [
         {
           "tileSize": 256,
@@ -32,7 +63,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Kitchen cabinet",
+  "name": "Tracon Kitchen Cabinet",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
